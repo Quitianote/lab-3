8ec57ef2e2e543fcd8acaa499a117cd6d47061ea
Quitianote <santiago.quitian@udea.edu.co>
2023-04-03 10:32:08 -0500

    metodo 1

--- practica_3/metodo 1/pract.cpp
+++ practica_3/metodo 1/pract.cpp
@@ -3,17 +3,38 @@
 
 using namespace std;
 
+int conv(char);
+
+
 int main(){
-    ifstream users;
+    ifstream texto;
+    char temp[256];
+    int numtext = 0;
+    char nom[60] = "";
 
-    users.open("usuarios.txt");
+    cout << "Ingrese nombre del archivo: "; cin >> nom;
+    cout << nom << endl;
 
-    if(users.is_open())cout << "esta abierto" << endl;
+    texto.open(nom);
+
+    if(texto.is_open())cout << "esta abierto" << endl;
     else cout << "esta cerrado" << endl;
     
-    users.close();
-    
+    while(texto.good()){
+        temp = texto.get();
+        if(texto.good())numtext = numtext*10 + (temp - '0');
+
+
+
+    }
+
+    texto.close();
+
     return 0;
 }
 
+int conv(char texto){
 
+
+
+}
