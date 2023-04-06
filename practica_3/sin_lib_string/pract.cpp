69df2bc5285692cd99de6973796e9a33443a4480
Quitianote <santiago.quitian@udea.edu.co>
2023-04-05 22:41:01 -0500

    metodo 1 finalizado al fin :D

--- practica_3/sin_lib_string/pract.cpp
+++ practica_3/sin_lib_string/pract.cpp
@@ -29,7 +29,9 @@ int main(){
     cout << "Ingrese nombre del archivo binario: "; cin >> bin;
     cout << "Ingrese semilla: "; cin >> n;
     cout << "Ingrese metodo de codificacion: "; cin >> codi;
-    
+    while(codi != 1 && codi != 2){
+        cout << "//ERROR//::Ingrese metodo de codificacion 1 o 2: "; cin >> codi;
+    }
     texto.open(nom);
     binario.open(bin, ofstream::binary);
 
@@ -40,6 +42,7 @@ int main(){
     
     while(texto.good()){//leer texto y pasar a entero
         char temp = texto.get();
+
         if((temp - 0) != (-1)){
             caracter[i] = (temp - 0);
             i ++;
@@ -49,14 +52,13 @@ int main(){
     imprimir(caracter, i);
     conv(caracter, i, escri);
     imprimir(caracter, i);
-    cout << escri[0] << "escri esta aca 1" << endl;
+
     if(codi == 1){
         codi1(escri, n, i);
     }
-    cout << escri[0] << "escri esta aca 2" << endl;
-    //for(; cont < i; cont ++){
+
     binario << escri[0];
-    //}
+    
     texto.close();
     binario.close();
 
@@ -78,6 +80,7 @@ void conv(int cadena[], int tam, string escri[]){//pasar de numero a binario
     for(; i < tam; i++){
         let = cadena[i];
         divi = let/2;
+
         while(divi != (1/2)){
             divi = let/2;
             resi = float(let%2);
@@ -86,12 +89,14 @@ void conv(int cadena[], int tam, string escri[]){//pasar de numero a binario
             cont ++;
         }
         cont1 = cont;
+
         while(cont > 0){
             num *= 10;
             cont --;
         }
         ss << int(num);
         str = ss.str();
+
         while(cont1 < 8){
             str.insert(0, "0");//inserto en la primera posicion el caracter "0"
             cont1 ++;
@@ -102,17 +107,15 @@ void conv(int cadena[], int tam, string escri[]){//pasar de numero a binario
         cont1= 0;
         num = 0;
         ss.str("");//vaciando ss
-        cout << escri[0] << " escri creandose" << endl;
     }
 }
 
 void imprimir(int caracter[], int i){
     int cont = 0;
+
     for(; cont < i; cont ++){
         cout << caracter[cont] << endl;
-
     }
-
 }
 /*
 void arrstr(string str, int tam){//convirtiendo arreglo en un string
@@ -140,19 +143,18 @@ void codi1(string escri[], int n, int tam){//metodo de codificacion 1
     int cont1 = 0;
     int con0 = 0;
     char temp[256] = "";
-    cout << "tamaño " << tam << endl;
+
     for(; i < tam*8; i ++){//entrando bit por bit, tam*8 es igual a la cantidad de bits, ya que tam son los bytes
         if(i == 0){//primer cambio de semilla
             cambit1(escri, n);
             continue;
         }
         else if(i == semi){//verificar si i es igual a la semilla
+            if(i + n > tam*8){//mirando si i + n(o sea la variable final) se pasa del rango
+                n = tam*8 - i;
+            }
             codi1cam(escri, tam, i, i + n);//codificando la semilla n
             semi += n;
-            if(n > tam*8){
-                n = (tam*8) - n;
-            }
-            cout << escri[0] << " escri estuvo por codificacion1" << endl;
         }
         cout << "i: " << i  << " n: " << n << endl;
     }
@@ -162,13 +164,12 @@ void codi1(string escri[], int n, int tam){//metodo de codificacion 1
 void cambit1(string escri[], int n){//primer cambio de bit codi1    
     int i = 0;
     int bit = 0;
-    cout << escri[0] << " escri estuvo por cambio 1" << endl;
+
     for(; i < n; i ++){
         bit = escri[0][i];
         if(bit == '1')escri[0][i] = '0';
         else escri[0][i] = '1';
     }
-    cout << escri[0] << " escri estuvo por cambio 1 por segunda vez" << endl;
 }
 
 void codi1cam(string cad[], int tam, int ini, int fin){//codificacion 1
@@ -177,15 +178,14 @@ void codi1cam(string cad[], int tam, int ini, int fin){//codificacion 1
     int cont = 1;
     int cont0 = 0;
     int cont1 = 0;
-    cout << cad[0] << " escri estuvo por codificacion 2" << endl;
-    cout << ini << " inicio " << fin << " fin " << endl;
+
     for(; ini < fin; ini ++){//contando 0 y 1
         if(cad[0][ini] == '0')cont0 ++;
         else cont1 ++;
     }
     i = 0;
     ini = inicop;
-    cout << cont0 << " contador0 " << cont1 << " contador1 " << endl;
+
     if(cont1 == cont0){//primera condicion
         for(; ini < fin; ini ++){
             switch(cad[0][ini]){
@@ -198,7 +198,7 @@ void codi1cam(string cad[], int tam, int ini, int fin){//codificacion 1
             }
         }
     }
-    cout << cad[0] << " escri estuvo por codificacion pero analizando contadores" << endl;
+
     if(cont0 > cont1){//segunda condicion
         for(; ini < fin; ini ++){
             if(cont%2 == 0){
@@ -215,6 +215,7 @@ void codi1cam(string cad[], int tam, int ini, int fin){//codificacion 1
         }
     }
     cout << "contador: " << cont << endl;
+
     if(cont1 > cont0){//tercera condicion
         for(; ini < fin; ini ++){
             if(cont%3 == 0){
@@ -230,7 +231,6 @@ void codi1cam(string cad[], int tam, int ini, int fin){//codificacion 1
             cont ++;
         }
     }
-    cout << cad[0] << " escri estuvo por codificacion 3" << endl;
 }
 /*
 void codi2(string escri[], int n){
