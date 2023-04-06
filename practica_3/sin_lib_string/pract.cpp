ad4c322914be861ffd81c8be70bc21904837a1d9
Quitianote <santiago.quitian@udea.edu.co>
2023-04-05 20:23:02 -0500

    tratando de acabar ejerciciosin string

--- practica_3/sin_lib_string/pract.cpp
+++ practica_3/sin_lib_string/pract.cpp
@@ -4,11 +4,12 @@
 
 using namespace std;
 
-void conv(int*, int, string*);
+void conv(int*, int, string);
 void imprimir(int*, int);
-void codi1(string* , int, int);
-void codi2(string* , int, int);
-void cambit1(string*, int);
+string codi1(string , int, int);
+string codi1cam(string , int, int, int);
+void codi2(string , int, int);
+string cambit1(string, int);
 
 
 int main(){
@@ -22,7 +23,7 @@ int main(){
     char nom[60] = "";
     char bin[60] = "";
     int caracter[256];
-    string escri[256];
+    string escri;
 
     cout << "Ingrese nombre del archivo: "; cin >> nom;
     cout << "Ingrese nombre del archivo binario: "; cin >> bin;
@@ -37,7 +38,7 @@ int main(){
     if(binario.is_open())cout << "esta abierto binario" << endl;
     else cout << "esta cerrado binario" << endl;
     
-    while(texto.good()){
+    while(texto.good()){//leer texto y pasar a entero
         char temp = texto.get();
         caracter[i] = (temp - 0);
         i ++;
@@ -47,19 +48,20 @@ int main(){
     conv(caracter, i, escri);
     imprimir(caracter, i);
 
-    //if(codi == 1)codi1(escri, n, i);
-
-    for(; cont < i; cont ++){
-        binario << escri[cont];
+    if(codi == 1){
+        escri = codi1(escri, n, i);
     }
-    
+
+    //for(; cont < i; cont ++){
+    binario << escri;
+    //}
     texto.close();
     binario.close();
 
     return 0;
 }
 
-void conv(int cadena[], int tam, string escri[]){//pasar de numero a binario
+void conv(int cadena[], int tam, string escri){//pasar de numero a binario
     float num = 0;
     float resi = 0;
     int cont = 0;
@@ -86,21 +88,19 @@ void conv(int cadena[], int tam, string escri[]){//pasar de numero a binario
             num *= 10;
             cont --;
         }
-        str = to_string(int(num));
         ss << int(num);
         str = ss.str();
         while(cont1 < 8){
             str.insert(0, "0");//inserto en la primera posicion el caracter "0"
             cont1 ++;
         }
-        escri[0] = str.insert(8*i, str);
+        escri.insert(8*i, str);
         cadena[i] = num;
         cont = 0;
         cont1= 0;
         num = 0;
         ss.str("");//vaciando ss
     }
-
 }
 
 void imprimir(int caracter[], int i){
@@ -111,88 +111,120 @@ void imprimir(int caracter[], int i){
     }
 
 }
-
-void arrstr(string str[], int tam){//convirtiendo arreglo en un string
+/*
+void arrstr(string str, int tam){//convirtiendo arreglo en un string
     int i = 0;
     int let = 0;
     int pos = 1;
-    string cad[0] = str[0];//copiando arreglo
+    int elem = 0;
+    string cad = str;//copiando arreglo
     for(; i < tam; i ++){
+        str[i] = "";
         for(; let < 8; let ++){
-            str[i][let] = str[i].insert(pos,cad[0][let]);
+            str[i] = str[i].insert(pos, 1, cad[elem]);
+
             
         }
 
 
     }
 }
-
-void codi1(string escri[], int n, int tam){
+*/
+string codi1(string escri, int n, int tam){//metodo de codificacion 1
     int i = 0;
     int dif = 0;
-    int let = 0;
+    int semi = n;//copia de semilla
     int cont1 = 0;
     int con0 = 0;
     char temp[256] = "";
-    if(n > 8){
-        dif = n-8;
+    for(; i < tam*8; i ++){//entrando bit por bit, tam*8 es igual a la cantidad de bits, ya que tam son los bytes
+        if(i == 0){//primer cambio de bit
+            escri = cambit1(escri, n);
+            continue;
+            //break;
+        }
+        else if(i ++ == n){//verificar si let + 1 es igual a la semilla, ya que si comparo let == n, lo estaria contando a let desde 1, no desde 0
+            escri = codi1cam(escri, tam, i, i + n);//codificando la semilla n
+            n = n + semi;
+            if(n > tam*8){
+                n = (tam*8) - n;
+            }
+            
+        }
     }
-    for(; i < tam; i ++){//entrando a byte
-        if(i == 8){
-            i = n - 8;
-        }
-        for(; let < 8; let ++){//entrando a bit
-            if(i == 0){//primer cambio de bit
-                cambit1(escri, n);
-                continue;
-                //break;
-            }
-            if(let + 1 == n){
+    return escri;
+}   
 
 
-            }
-            temp[let] = escri[i][let];
-
-
-
-        }
-        
-        break;
-    }    
-}
-
-void cambit1(string escri[], int n){//primer cambio de bit codi1    
+string cambit1(string escri, int n){//primer cambio de bit codi1    
 
     int i = 0;
     int bit = 0;
     for(; i < n; i ++){
-        bit = escri[0][i];
-        if(bit == '1')escri[0][i] = '0';
-        else escri[0][i] = '1';
+        bit = escri[i];
+        if(bit == '1')escri[i] = '0';
+        else escri[i] = '1';
     }
+    return escri;
 }
 
-void codi1cam(char cad[], int tam){
+string codi1cam(string cad, int tam, int ini, int fin){//codificacion 1
     int i = 0;
+    int inicop = ini;
+    int cont = 1;
     int cont0 = 0;
     int cont1 = 0;
-
-
-    for(; i < tam; i ++){
-        if(cad[i] == '0')cont0 ++;
+    for(; ini <= fin; ini ++){
+        if(cad[ini] == '0')cont0 ++;
         else cont1 ++;
     }
-    if(cont1 == cont)
-
+    i = 0;
+    ini = inicop;
+    for(cont1 == cont0; ini <= fin; ini ++){
+        switch(cad[ini]){
+            case '1':
+                cad[ini] = '0';
+                break;
+            case '0':
+                cad[ini] = '1';
+                break;  
+        }
+    }
+    for(cont0 > cont1; ini <= fin; ini ++){
+        if(cont%2 == 0){
+            switch(cad[ini]){
+                case '1':
+                    cad[ini] = '0';
+                    break;
+                case '0':
+                    cad[ini] = '1';
+                    break;  
+            }
+        }
+        cont ++;
+    }
+    for(cont1 > cont0; ini <= fin; ini ++){
+        if(cont%3 == 0){
+            switch(cad[ini]){
+                case '1':
+                    cad[ini] = '0';
+                    break;
+                case '0':
+                    cad[ini] = '1';
+                    break;  
+            } 
+        }
+    }
+    return cad;
 }
-
+/*
 void codi2(string escri[], int n){
 
 
 
 
 }
-
+*/
 
 
 
